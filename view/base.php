522b4753f92b4e01bc0ca4ade4d9b06dc39c792a
Florent Maury <flo.neo@hotmail.fr>
2023-08-16 12:24:41 +0200

    Corrections.

--- view/base.php
+++ view/base.php
@@ -141,7 +141,7 @@
             </div>
         </footer>
 
-        <!-- JQUery animation et AJAX. -->
+        <!-- JQuery animation et AJAX. -->
 
         <script
             src="https://code.jquery.com/jquery-3.7.0.min.js"
@@ -157,12 +157,14 @@
 
         <!-- Script JavaScript pour animations (ex : modales) Bootstrap. -->
 
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
+        <script 
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
             integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" 
             crossorigin="anonymous">
         </script>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" 
+        <script 
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" 
             integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" 
             crossorigin="anonymous">
         </script>
